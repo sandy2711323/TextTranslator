263ae2b65e048fba506a35403b248e9918b93d62
संदीप सटोणे( Sandeep Satone ) <sandy2711323@gmail.com>
2016-09-24 22:08:50 +0530

    Added placeholder for the textArea . Added the code for bing translator Need to generate the App ID and replace with value in URL to check the functionality

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function clearTranslate() {
-    document.getElementById('clear').innerHTML = "";
+    document.getElementById('clear').value = "";
+    document.getElementById('translate').value = "";
+}
+
+// Still working on code 
+/*var languageFrom = document.getElementById("langpairFROM").value;
+var languageTo = document.getElementById("langpairTO").value;*/
+var s = document.createElement("script");
+/*s.src = "http://api.microsofttranslator.com/V2/Ajax.svc/Translate?oncomplete=mycallback&appId=68D088969D79A8B23AF8585CC83EBA2A05A97651&from=" + languageFrom + "&to=" + languageTo + "&text=" + text;*/
+s.src = "http://api.microsofttranslator.com/V2/Ajax.svc/Translate?oncomplete=mycallback&appId=68D088969D79A8B23AF8585CC83EBA2A05A97651&from=" + "English" + "&to=" + "Russian" + "&text=" + "Hello Sandeep";
+
+function translator() {
+    var text = document.getElementById('clear').value;
+    window.mycallback = function (response) {
+        document.getElementById('translate').innerHTML = response;
+    }
 }
