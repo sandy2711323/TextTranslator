e031c01706d7d6f3f36798b9a1c0b13ad70aaa87
संदीप सटोणे( Sandeep Satone ) <sandy2711323@gmail.com>
2016-09-26 15:35:55 +0530

    Completed the Project .

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-function clearTranslate() {
-    document.getElementById('text').value = "";
-    document.getElementById('translatedText').value = "";
-}
-
-/*function translator() {
-    var text = document.getElementById('text').value;
-    window.mycallback = function (response) {
-        document.getElementById('translatedText').innerHTML = response;
+    function clearTranslate() {
+        document.getElementById('text').value = "";
+        document.getElementById('translatedText').value = "";
     }
-}*/
 
+    function translator(str) {
+       
+        if (str.length == 0) { 
+            document.getElementById("text").innerHTML = "";
+            return;
+        } else {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    
 
-function translator(str) {
-   
-    if (str.length == 0) { 
-        document.getElementById("text").innerHTML = "";
-        return;
-    } else {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("translatedText").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET", "translate.php?q=" + str, true);
-        xmlhttp.send();
+                    document.getElementById("translatedText").innerHTML = this.responseText.replace(/<(?:.|\n)*?>/gm, '').trim();
+                }
+            };
+            xmlhttp.open("GET", "translate.php?q=" + str, true);
+            xmlhttp.send();
+        }
+        
     }
- 
-}
