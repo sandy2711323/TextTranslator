edbaebd48aeaef2aa312056da741cb42049fe4ca
संदीप सटोणे( Sandeep Satone ) <sandy2711323@gmail.com>
2016-10-03 22:12:44 +0530

    Removed the unwanted code from the index.js file

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-    function clearTranslate() {
-        document.getElementById('text').value = "";
-        document.getElementById('translatedText').value = "";
-    }
-
     function translator(str) {
-       
-        if (str.length == 0) { 
+        if (str.length == 0) {
             document.getElementById("text").innerHTML = "";
             return;
-        } else {
+        }
+        else {
             var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function() {
+            xmlhttp.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200) {
-                    
-
                     document.getElementById("translatedText").innerHTML = this.responseText.replace(/<(?:.|\n)*?>/gm, '').trim();
                 }
             };
             xmlhttp.open("GET", "translate.php?q=" + str, true);
             xmlhttp.send();
         }
-        
     }
