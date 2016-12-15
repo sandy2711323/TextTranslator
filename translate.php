3d66b58284936ab52b113f6735827c58a81d0e90
संदीप सटोणे( Sandeep Satone ) <sandy2711323@gmail.com>
2016-12-15 11:22:06 +0530

    Removed the id and key

--- translate.php
+++ translate.php
@@ -10,8 +10,8 @@
     <?php
 require_once('translate.class.php');
 
-//$BingTranslator = new BingTranslator('<Application ID>', '<Application Secret>');
-$BingTranslator = new BingTranslator('1684274a-92f4-4a0c-b932-99482dadf51a', '2eGe60XtmBXh4uxt54qjvB+PKiN2EkRBEUatJRfDblQ=');
+$BingTranslator = new BingTranslator('<Application ID>', '<Application Secret>');
+
 $q = $_REQUEST["q"];
     if ($q !== "") {
     $translation = $BingTranslator->getTranslation('en', 'hi', $q);
