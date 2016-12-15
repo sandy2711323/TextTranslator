47997c819e451480960e60cea082792190e015d8
संदीप सटोणे( Sandeep Satone ) <sandy2711323@gmail.com>
2016-12-15 11:17:52 +0530

    Added text to speach feature in the converter

--- index.js
+++ index.js
@@ -10,7 +10,26 @@
                     document.getElementById("translatedText").innerHTML = this.responseText.replace(/<(?:.|\n)*?>/gm, '').trim();
                 }
             };
-            xmlhttp.open("GET", "translate.php?q=" + str, true);
+            //xmlhttp.open("GET", "translate.php?q=" + str, true);
+            xmlhttp.open("GET", "http://sandeepbalramsatone.com/texttranslator/translate.php?q=" + str, true);
             xmlhttp.send();
+        }
+    }
+    // This will translate from voice to text
+    function startDictation() {
+        if (window.hasOwnProperty('webkitSpeechRecognition')) {
+            var recognition = new webkitSpeechRecognition();
+            recognition.continuous = false;
+            recognition.interimResults = false;
+            recognition.lang = "en-US";
+            recognition.start();
+            recognition.onresult = function (e) {
+                document.getElementById('technosandy').value = e.results[0][0].transcript;
+                recognition.stop();
+                translator(document.getElementById('technosandy').value)
+            };
+            recognition.onerror = function (e) {
+                recognition.stop();
+            }
         }
     }
